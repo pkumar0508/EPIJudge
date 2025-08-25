eedc95ca79ce01e3580860b6c96d5f3dccf93c55
Prashant Kumar <pkumar0508@gmail.com>
2025-08-25 07:32:21 -0400

    String Integer Interconversion

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,13 +3,38 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    if x == 0:
+        return '0'
+
+    sign = ''
+    if x < 0:
+        sign = '-'
+        x = -1 * x
+
+    out = []
+    # assert(x > 0)
+    while x:
+        out.append(chr(ord('0') + x % 10))
+        x = x // 10
+    
+    out.append(sign)
+    return ''.join(out[::-1])
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    start = 0
+    sign = 1
+    if s[0] == '-':
+        sign = -1
+        start = 1
+    elif s[0] == '+':
+        start = 1
+
+    out = 0
+    for c in s[start:]:
+        digit = ord(c) - ord('0')
+        out = 10 * out + digit
+    return sign * out
 
 
 def wrapper(x, s):
