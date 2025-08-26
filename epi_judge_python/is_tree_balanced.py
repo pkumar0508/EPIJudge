471fe343a86d049aefa5bccf20a48d98dc19e09b
Prashant Kumar <pkumar0508@gmail.com>
2025-08-25 20:19:50 -0400

    Binary Tree is Height Balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,10 +1,30 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+from typing import Optional
 
+
+def height(node: Optional[BinaryTreeNode]) -> tuple[int, bool]:
+    if not node:
+        return 0, True
+
+    # check if left child is balanced
+    left, left_ok = height(node.left)
+    if not left_ok:
+        return 0, False
+
+    # check if right child is balanced
+    right, right_ok = height(node.right)
+    if not right_ok:
+        return 0, False
+
+    # check heights
+    new_height = max(left, right) + 1
+    balanced = abs(left - right) < 2
+    return new_height, balanced
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    _, balanced = height(tree)
+    return balanced
 
 
 if __name__ == '__main__':
