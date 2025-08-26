90c5e1f4ae10c9c0d705a7a9fa24ea587a4cc302
Prashant Kumar <pkumar0508@gmail.com>
2025-08-25 20:23:18 -0400

    Merge Sorted Files

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,26 @@
+from heapq import heappop, heappush, heapify
 from typing import List
 
 from test_framework import generic_test
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    heap = []
+    def push(values: List[int]) -> None:
+        if values:
+            pair = values[0], values[1:]
+            heappush(heap, pair)
+
+    for item in sorted_arrays:
+        push(item)
+    
+    out = []
+    while heap:
+        head, tail = heappop(heap)
+        push(tail)
+        out.append(head)
+
+    return out
 
 
 if __name__ == '__main__':
