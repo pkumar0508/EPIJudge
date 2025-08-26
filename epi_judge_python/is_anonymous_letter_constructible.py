0b28ee5618725e79b3ace7334796729e7c04a850
Prashant Kumar <pkumar0508@gmail.com>
2025-08-25 23:15:57 -0400

    Is anonymous letter constructible

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -3,8 +3,23 @@ from test_framework import generic_test
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    counts = {}
+    for letter in letter_text:
+        if letter not in counts:
+            counts[letter] = 0
+        counts[letter] += 1
+
+    for letter in magazine_text:
+        if letter not in counts:
+            continue
+
+        counts[letter] -=1
+        if not counts[letter]:
+            del counts[letter]
+        if not counts:
+            break
+
+    return not counts
 
 
 if __name__ == '__main__':
