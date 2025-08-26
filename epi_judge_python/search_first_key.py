d46d498127e6cc578a7a0b964af27930ee2feebc
Prashant Kumar <pkumar0508@gmail.com>
2025-08-25 21:45:30 -0400

    Search a sorted array for the first occurrance of k

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -4,8 +4,23 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    start = 0
+    end = len(A)
+
+    while start != end:
+        # assert all([
+        #     all(x < k for x in A[:start]),
+        #     all(x >= k for x in A[end:]),
+        # ])
+        mid = start + (end - start) // 2
+        if A[mid] < k:
+            start = mid + 1
+        else:
+            end = mid
+            
+    if start == len(A) or A[start] != k:
+        return -1
+    return start
 
 
 if __name__ == '__main__':
