839062dc4e6abe0efe46fada40c59748fc837fa7
Prashant Kumar <pkumar0508@gmail.com>
2025-08-28 09:46:39 -0400

    Parity of a word

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,11 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    count = 0
+    while x:
+        x &= (x - 1)
+        count ^= 1
+    return count % 2
 
 
 if __name__ == '__main__':
